8fbe8394a842368d750c69604838c1c675702db0
Esaú Medina <esau0204@gmail.com>
2026-01-20 13:06:36 -0300

    Conectar app con Google Drive como backend

--- app.py
+++ app.py
@@ -2,10 +2,13 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from editor import CatalogoEditor
+from storage import DriveStorage
+
+FILE_ID = "1yu0nemxng0i4Qc_rlTnx7AackuJbebJX"  # <-- tu fileId de Drive
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    # Puedes cambiar el nombre/ruta del CSV o del ícono aquí si hace falta
-    ventana = CatalogoEditor(csv_path="catalogo_inicial.csv", lupa_icon="lupa.png")
+    storage = DriveStorage(file_id=FILE_ID, credentials_path="credentials.json", token_path="token.json")
+    ventana = CatalogoEditor(storage=storage, lupa_icon="lupa.png")
     ventana.show()
     sys.exit(app.exec_())
