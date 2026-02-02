a4b203abd9dd73dd167e90380e14b5ce9013f3fd
Esaú Medina <esau0204@gmail.com>
2026-02-02 11:58:15 -0300

    secrets debug

--- app_web.py
+++ app_web.py
@@ -1,12 +1,5 @@
 import streamlit as st
-import json
-
-st.write("Secrets keys:", st.secrets.keys())
-
-if "google" in st.secrets:
-    st.write("Google secret found")
-    st.write("Google keys:", st.secrets["google"].keys())
-
+#import json
 
 import pandas as pd
 import unicodedata
