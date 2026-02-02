9803f6f75dd1b21fc5bc0e338f541e1ef6d0a4a2
Esaú Medina <esau0204@gmail.com>
2026-02-02 11:56:26 -0300

    service account

--- app_web.py
+++ app_web.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import json
 
-#st.write("Secrets keys:", st.secrets.keys())
+st.write("Secrets keys:", st.secrets.keys())
 
-i#f "google" in st.secrets:
-#    st.write("Google secret found")
-#    st.write("Google keys:", st.secrets["google"].keys())
+if "google" in st.secrets:
+    st.write("Google secret found")
+    st.write("Google keys:", st.secrets["google"].keys())
 
 
 import pandas as pd
