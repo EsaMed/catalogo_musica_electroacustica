a1b10804d0be9b67ed367f77f76b1b5de35f86df
Esaú Medina <esau0204@gmail.com>
2026-02-06 04:35:46 -0300

    Mejouras UI

--- app_web.py
+++ app_web.py
@@ -34,6 +34,16 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
+# ---BLOQUEAR BARRA LATERAL ---
+st.markdown("""
+    <style>
+    /* Oculta la flecha/botón para colapsar el sidebar */
+    [data-testid="stSidebarCollapsedControl"] {
+        display: none;
+    }
+    </style>
+""", unsafe_allow_html=True)
+
 FILE_ID = "1yu0nemxng0i4Qc_rlTnx7AackuJbebJX"
 
 @st.cache_resource
